cccc33b3f11462d345d762e9e15799e751c4c64e
Mirza <81905418+mirza-hrustanovic@users.noreply.github.com>
2023-11-12 23:44:58 +0100

    added sized box and divider as child below icon and two text,something like horiyontal line

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -11,91 +11,81 @@ class MyApp extends StatelessWidget {
       backgroundColor: Colors.teal,
       //SafeArea is just widget that we name it like that
       body: SafeArea(
-        child:Column(
-          children:<Widget> [
-          CircleAvatar(
-            radius: 50,
-            backgroundColor: Colors.red,
-            backgroundImage:AssetImage("images/profile_n.jpg") ,
-          ),
-            Text("Mirza Hrustanović",
-              style: TextStyle(
-                fontFamily:"Pacifico",
-                color: Colors.white,
-                fontSize: 33,
-                fontWeight:FontWeight.bold
-              ),
-
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.red,
+              backgroundImage: AssetImage("images/profile_n.jpg"),
             ),
-            Text("FLUTTER DEVELOPER",
+            Text(
+              "Mirza Hrustanović",
               style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontFamily:"Source Sans 3",
-                fontWeight:FontWeight.bold,
-                letterSpacing: 3
-
-              ),
-
+                  fontFamily: "Pacifico",
+                  color: Colors.white,
+                  fontSize: 33,
+                  fontWeight: FontWeight.bold),
             ),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25),
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children:<Widget> [
-                  Icon(
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: "Source Sans 3",
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3),
+            ),
+            SizedBox(
+              height: 20,
+              width: 250,
+              //vertical line divider
+              child:Divider(
+                color:Colors.teal.shade100
+              ) ,
+            ),
+            Card(
+
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: ListTile(
+                  leading:Icon(
                     Icons.phone,
                     color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text("061/636-832",
-                    style: TextStyle(
-                      color: Colors.teal.shade600,
-                      fontSize: 20,
-                      fontFamily: "Source Sans 3"
-                    ),
-
-                  ),
-
-                ],
-
-              ),
-
-
-            ),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25),
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children:<Widget> [
-                  Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text("mirza-hrustan@mail.com",
+                  ) ,
+                  title:Text(
+                    "061/636-832",
                     style: TextStyle(
                         color: Colors.teal.shade600,
                         fontSize: 20,
-                        fontFamily: "Source Sans 3"
-                    ),
-
-                  ),
-
-                ],
-
+                        fontFamily: "Source Sans 3"),
+                  ) ,
+                )
               ),
+            ),
+            Card(
 
-
-            )
-
-
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child:
+                  ListTile(
+                   leading: Icon(
+                     Icons.email,
+                     color: Colors.teal,
+                   ),
+                    title:
+                    Text(
+                      "mirza-hrustan@mail.com",
+                      style: TextStyle(
+                          color: Colors.teal.shade600,
+                          fontSize: 20,
+                          fontFamily: "Source Sans 3"),
+                    ),
+                  ),
+              ),
+            ),
           ],
         ),
       ),
